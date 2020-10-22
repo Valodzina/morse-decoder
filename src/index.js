0d342eacd495809a911aa13955c5b67a86762845
Ulyana Valodzina <67861703+Valodzina@users.noreply.github.com>
2020-10-22 09:05:45 +0300

    unit: solution

--- src/index.js
+++ src/index.js
@@ -38,6 +38,52 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+
+    let ans="";
+    let arr=new Array();
+    arr= expr.split('**********');
+    let arr1=new Array();
+    let t="";
+    const length = 10; // длина одной части
+    const pattern = new RegExp(".{1," + length + "}", "ig");
+    let tt="";
+
+    for(let i=0; i<arr.length;i++) //массив слов
+    {
+       
+        arr1 = arr[i].match(pattern).map(item => item.padEnd(length, ".")); //массив букв
+        //return arr1.length;
+        for(let k=0; k<arr1.length ;k++)// определяем каждую букву
+        {
+            t="";
+            tt="";
+            let j=0;
+
+            //return arr1[k].length;
+         //смотрим содержимое каждой буквы
+        while(j<10)
+        {
+             if(arr1[k][j]==="1"&& arr1[k][j+1]==="1")
+             tt+="-";
+             if(arr1[k][j]==="1"&& arr1[k][j+1]==="0")
+              tt+=".";
+
+             j+=2;
+           
+        }
+         t=MORSE_TABLE[tt];
+         ans+=t;
+         }
+
+
+
+    
+    if (i!=arr.length-1)
+    ans+=" ";
+    }
+    return ans;
+    //return MORSE_TABLE[expr[0]];
+   // console.log(expr);
     // write your solution here
 }
 
